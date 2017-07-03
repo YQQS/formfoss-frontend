6ec5872491e22f8e38c9b9bcca822481f8ec1cf2
aceforeverd <dace1p@gmail.com>
2017-07-03 17:44:01 +0800

    update prototype

--- src/app/component/form-prototype/form-prototype.component.ts
+++ src/app/component/form-prototype/form-prototype.component.ts
@@ -3,20 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-form-prototype',
     templateUrl: './form-prototype.component.html',
-    styleUrls: ['./form-prototype.component.css']
+    styleUrls: ['./form-prototype.component.scss']
 })
 export class FormPrototypeComponent implements OnInit {
     animals: string[] = [
-        "dog",
-        "cat",
-        "bird",
-        "monkey"
+        'dog',
+        'cat',
+        'bird',
+        'monkey',
+        'ant'
     ];
-    selectedAnimal = "";
+    selectedAnimal = '';
+    animalList: string[] = [];
+    counts = 0;
+    words: string = '';
+    selectedDate: Date;
+
+    updateMultiSelect(elements) {
+        this.animalList = [];
+        console.log(elements);
+        for (let i = 0; i < elements.options.length; i++) {
+            this.animalList.push(elements.options.indexOf(i));
+        }
+    }
 
     constructor() { }
 
+    updateCount(): void {
+        this.counts = this.words.length;
+    }
     ngOnInit() {
     }
-
 }
